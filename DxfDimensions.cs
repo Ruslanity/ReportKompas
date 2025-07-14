e3f7e697a1516e90011e767c050511eee31fe51f
Ruslanity <ruslanity@yandex.ru>
2025-07-14 09:56:16 +0300

    fix bugs

---
+++ DxfDimensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using netDxf;
+using netDxf.Entities;
+
+namespace ReportKompas
+{
+    public class DxfDimensions
+    {
+        public double MinX { get; set; }
+        public double MaxX { get; set; }
+        public double MinY { get; set; }
+        public double MaxY { get; set; }
+        public double MinZ { get; set; }
+        public double MaxZ { get; set; }
+
+        public DxfDimensions()
+        {
+            MinX = double.MaxValue;
+            MaxX = double.MinValue;
+            MinY = double.MaxValue;
+            MaxY = double.MinValue;
+            MinZ = double.MaxValue;
+            MaxZ = double.MinValue;
+        }
+    }
+}
